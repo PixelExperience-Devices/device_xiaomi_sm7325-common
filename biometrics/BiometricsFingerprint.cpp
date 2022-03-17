89f9f163005012e926bee1a9ce58b212d2a4e383
IPSBHANGU <inderpreetbhangu2@gmail.com>
2022-03-17 05:51:48 +0100

    sm7325-common: biometrics: Add goodix HAL

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -37,6 +37,7 @@ static const uint16_t kVersion = HARDWARE_MODULE_API_VERSION(2, 1);
 
 // List of fingerprint HALs
 static const char *kHALClasses[] = {
+    "goodix",
     "fpc",
     "goodix_fod",
     "goodix_fod6",
