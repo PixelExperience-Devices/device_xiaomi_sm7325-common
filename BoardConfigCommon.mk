4ba5d554c83cbfe08798054d57f10c426c4dcaec
kattya9512 <kartiklashkare@gmail.com>
2023-08-01 19:06:26 +0530

    sm7325-common: Allow BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,6 +172,7 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # Soong
 SOONG_CONFIG_NAMESPACES += xiaomiSm7325Vars
