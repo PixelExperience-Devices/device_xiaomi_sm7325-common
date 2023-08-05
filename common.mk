ea75ffe93c31a0c24db521da4d2d1d3586e0de75
kattya9512 <kartiklashkare@gmail.com>
2023-08-05 18:04:01 +0530

    sm7325-common: hidl: Upgrade android.hardware.audio and android.hardware.audio.effect
        E LegacySupport: Could not get passthrough implementation for android.hardware.audio@7.1::IDevicesFactory/default
    
        E LegacySupport: Could not get passthrough implementation for android.hardware.audio.effect@7.0::IEffectsFactory/default

--- common.mk
+++ common.mk
@@ -44,8 +44,8 @@ PRODUCT_PACKAGES += \
     audio.usb.default
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.1-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl
 
