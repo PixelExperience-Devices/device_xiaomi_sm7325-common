38ba64456872ed2ac97f7a465f81ce4e665f2b2c
Chenyang Zhong <zhongcy95@gmail.com>
2023-09-16 22:39:21 +0530

    sm7325-common: init: Avoid migrating foreground tasks to prime The prime core is quite power hungry. Let's only use it for top-app tasks. Avoid migrating foreground tasks to prime to reduce unnecessary bumps of prime's frequency and save some battery.
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    
    Updated for our tree, also update background and remove duplicates
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- rootdir/bin/init.kernel.post_boot-yupik.sh
+++ rootdir/bin/init.kernel.post_boot-yupik.sh
@@ -186,7 +186,7 @@ echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 echo 0 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
 
 # cpuset parameters
-echo 0-3 > /dev/cpuset/background/cpus
+echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
 
 # configure governor settings for silver cluster
